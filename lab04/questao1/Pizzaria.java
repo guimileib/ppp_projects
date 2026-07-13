7c8a521d2c19c636c2ae2030f0c62d3c1b95f437
guimileib <guilhermemileib@gmail.com>
2026-07-13 08:23:31 -0300

    feat: Adjusted the Main in exercise1

--- lab04/questao1/Pizzaria.java
+++ lab04/questao1/Pizzaria.java
@@ -3,15 +3,16 @@ package lab04.questao1;
 public class Pizzaria {
     public static void main(String[] args) {
         // pedindo um tipo de pizza com massa fina
-        Pizza pedido1 =  new MassaFina();
+        Pizza marguerita =  new MassaFina();
         // "decorando", fazendo pizza Marguerita
-        pedido1 = new Queijo(pedido1);
-        pedido1 = new Tomate(pedido1);
+        marguerita = new Queijo(marguerita);
+        marguerita = new Tomate(marguerita);
 
-        System.out.println(pedido1.getDescricao());
+        System.out.println("Pizza: " + marguerita.getDescricao());
+        System.out.println("Valor: " + marguerita.getValor());
 
         // fazendo mostrando o decorator
         Pizza portuguesa = new Tomate(new Ovo(new Queijo(new MassaEspessa())));
-        System.out.println(portuguesa.getDescricao() + portuguesa.getValor());
+        System.out.println("\n" + "Pizza: " + portuguesa.getDescricao() + "\nValor: " + portuguesa.getValor());
     }
 }
